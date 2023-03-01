288b1ce4807d67a53cffd07b64f4eab592710f07
JungWoo <rlawjddn103@naver.com>
2023-03-01 23:59:44 +0900

    feat(LogInController): 로그인 기능 구현

---
+++ src/main/java/jwp/controller/LogInController.java
@@ -0,0 +1,38 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/users/loginForm","/users/login"})
+public class LogInController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+
+        MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
+        User user = memoryUserRepository.findUserById(userId);
+
+        if (user != null && user.getPassword().equals(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user",user);
+        }
+        resp.sendRedirect("/");
+
+    }
+}
