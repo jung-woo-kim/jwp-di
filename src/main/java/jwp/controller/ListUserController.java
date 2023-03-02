09d24ecbdb2cab525579747191a38b17610460ca
JungWoo <rlawjddn103@naver.com>
2023-03-02 23:19:40 +0900

    feat: 개인 정보 수정 세션id 있을 때만 가능하게끔 변경

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -1,6 +1,7 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.model.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +19,12 @@ public class ListUserController extends HttpServlet {
     private static Logger logger = Logger.getLogger(ListUserController.class.getName());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        if (value == null) {
+            resp.sendRedirect("/");
+            return;
+        }
         MemoryUserRepository userRepository = MemoryUserRepository.getInstance();
         req.setAttribute("users",userRepository.findAll());
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
--- src/main/java/jwp/controller/UpdateUserFormServlet.java
+++ src/main/java/jwp/controller/UpdateUserFormServlet.java
@@ -9,18 +9,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/users/updateForm")
 public class UpdateUserFormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userId = req.getParameter("userId");
-        MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
-        User user = memoryUserRepository.findUserById(userId);
 
-        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
-        req.setAttribute("user",user);
-        rd.forward(req,resp);
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        if (value != null) {
+            User user = (User) value;
+            if (user.getUserId().equals(req.getParameter("userId"))){
+                RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+                req.setAttribute("user",user);
+                rd.forward(req,resp);
+                return;
+            }
+            resp.sendRedirect("/user/list");
+            return;
+        }
+        resp.sendRedirect("/");
     }
 }
