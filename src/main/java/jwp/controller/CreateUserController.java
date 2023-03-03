2efacd3546a7d7ac56730a90bc8efac087d17384
JungWoo <rlawjddn103@naver.com>
2023-03-03 15:43:43 +0900

    feat(UserDao): 회원 가입 기능 구현

--- src/main/java/jwp/controller/CreateUserController.java
+++ src/main/java/jwp/controller/CreateUserController.java
@@ -1,19 +1,21 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.dao.UserDao;
 import jwp.model.User;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class CreateUserController implements Controller {
 
+    Logger logger = Logger.getLogger(CreateUserController.class.getName());
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = new User(
@@ -21,8 +23,12 @@ public class CreateUserController implements Controller {
                 request.getParameter("password"),
                 request.getParameter("name"),
                 request.getParameter("email"));
-        MemoryUserRepository userRepository = MemoryUserRepository.getInstance();
-        userRepository.addUser(user);
+        UserDao userDao = new UserDao();
+        try {
+            userDao.insert(user);
+        } catch (SQLException e) {
+            logger.log(Level.WARNING,e.getMessage());
+        }
         return "redirect:/user/list";
     }
 }
--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -1,11 +1,14 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.dao.UserDao;
 import jwp.util.HttpSessionUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -16,8 +19,12 @@ public class ListUserController implements Controller {
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         if (HttpSessionUtils.isLogined(session)) {
-            MemoryUserRepository userRepository = MemoryUserRepository.getInstance();
-            request.setAttribute("users",userRepository.findAll());
+            UserDao userDao = new UserDao();
+            try {
+                request.setAttribute("users",userDao.findAll());
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, e.getMessage());
+            }
             return "/user/list.jsp";
         }
         return "redirect:/users/loginForm";
